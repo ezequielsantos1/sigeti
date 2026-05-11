7b498926d3fd3ff92c705a4eadff7bf7d19dc565
Ezequiel Keslley Viana Santos da Cruz <ezequielcx14@gmail.com>
2026-05-11 17:24:33 -0300

    refactor(controller): corrigir a função index no DashbordController

--- app/Controllers/Admin/DashboardController.php
+++ app/Controllers/Admin/DashboardController.php
@@ -6,8 +6,10 @@ namespace App\Controllers\Admin;
 use App\Core\Controller;
 use App\Core\Auth;
 use App\Core\Permission;
+use App\Models\Department\Department;
 use App\Models\Role;
-use App\Models\Department;
+use App\Models\Ticket\Ticket;
+use App\Models\User;
 
 class  DashboardController extends Controller
 {
@@ -20,6 +22,21 @@ class  DashboardController extends Controller
 
     public function index(): void
     {
-        echo $this->view->render('admin/dashboard');
+        $totalUsers = (new User())->totalUsers();
+        $totalRoles = (new Role\Role())->totalRoles();
+        $totalDepartments = (new Department())->totalDepartments();
+        $totalOpenTickets = (new Ticket())->totalOpenTickets();
+        $recentUsers = (new User())->recentUsers();
+        $roles = (new Role\Role())->recentRoles();
+
+
+        echo $this->view->render('admin/dashboard', [
+            'totalUsers' => $totalUsers,
+            'totalRoles' => $totalRoles,
+            'totalDepartments' => $totalDepartments,
+            'totalOpenTickets' => $totalOpenTickets,
+            'recentUsers' => $recentUsers,
+            'roles' => $roles
+        ]);
     }
 }
